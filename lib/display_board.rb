1a947fe8c06b217c72b5fde4f8f2333f5a638652
luisandra abreu duran <abreuluisandra@gmail.com>
2020-05-19 07:46:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = 
+board =
   puts "   |   |   "
   puts "-----------"
   puts "   ||   ||   "
   puts "-----------"
   puts "   |   |   "
-
