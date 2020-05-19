4567f88db0b37838bca37c2329e0b236e8d0a183
luisandra abreu duran <abreuluisandra@gmail.com>
2020-05-19 08:11:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,3 @@ def display_board(board = ["O","X"," "," ","X"," ","X","O"," "])
   puts "#{board[3]} | #{board[4]} | #{board[5]}"
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
-
-
